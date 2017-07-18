99f2acd768b2a6c216149e6e2caf46908b5e9f77
gerson22 <gerson.isaias.rivera@gmail.com>
2017-07-18 18:57:07 -0500

    Ahora en el Config se puede agregar el layout de un input  File y el Form

--- Config.php
+++ Config.php
@@ -5,17 +5,30 @@ class Config {
 
     public static function Layout(){
         return (object)array(
-            'Input' => "<div class='md-form'>
-                           <i class='fa fa-icon prefix'></i>
-                           <input type=':type' id=':name' name=':name' required=':required' class='form-control'>
-                           <label for='form3'>:alias</label>
-                        </div>",
+            'Input' => "<div class=\"md-form input-group\">
+                          <span class=\"input-group-addon\" id=\"basic-addon1\"><span class='fa fa-:icon'></span></span>
+                          <input type=\":type\" id=':name' name=':name' class=\"form-control\" placeholder=\":alias\" aria-describedby=\"basic-addon1\">
+                      </div>",
             'Select' => "<div class='md-form'>
                             <select class='mdb-select colorful-select dropdown-warning' name=':name' id=':name'>
                                 <option val='NULL' selected>Elige una opción</option>
                             </select>
                             <label>:alias</label>
-                        </div>"
+                        </div>",
+            "File"  =>  "<form id='frm_:name'>
+                            <div class='file-field'>
+                                <div class='btn btn-warning btn-sm yellow-main'>
+                                    <span> :alias</span>
+                                    <input type='file' name=':name' id=':name'>
+                                </div>
+                                <div class='file-path-wrapper'>
+                                   <input class='file-path validate :name' type='text' placeholder='Nombre del archivo'>
+                                </div>
+                            </div>
+                        </form><br><br>",
+            'Form'  =>  "<form id=':name' method=':method' action=':action'>
+                            <div>:fields</div>
+                        </form>"
         );
     }
 
